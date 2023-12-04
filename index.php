3b594f969b2c77cae984be8bd40ed82825716007
DriveEasyMaster <152841919+DriveEasyMaster@users.noreply.github.com>
2023-12-04 10:09:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,1 @@
 
-<!DOCTYPE html HTML>
-<html>
-	<head>	
-		<meta content="text/html; charset=UTF-8">
-		<title>BALLET</title>
-	</head>
-	<body>				
-		<div>Hello world!</div>
-			
-	</body>
-</html>
